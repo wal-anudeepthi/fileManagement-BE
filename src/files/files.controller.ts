2965039540365367f1810aefe83387cd900a0a4c
wal-vishnuvardhan <vishnuvardhan.u@veltris.com>
2024-09-10 16:22:26 +0530

    upload file and delete file

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,11 +1,52 @@
-import { Controller, Body, Get, Param, Patch } from '@nestjs/common';
-import { join } from 'path';
+import {
+  Controller,
+  Body,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { extname, join } from 'path';
 import { FilesService } from './files.service';
 import { promises as fs } from 'fs';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import { UploadFileDto } from './dtos/upload-file.dto';
 
 @Controller('files')
 export class FilesController {
   constructor(private filesService: FilesService) {}
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          const prefix = `${file.originalname.split('.')[0]}-${uuidv4()}`;
+          const fileExt = extname(file.originalname);
+          const fileName = `${prefix}${fileExt}`;
+          callback(null, fileName);
+        },
+      }),
+    }),
+  )
+  uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() body: UploadFileDto,
+  ) {
+    return this.filesService.upload(file, body);
+  }
+
+  @Delete('/:id')
+  deleteFile(@Param('id') id: string, @Query() query: UploadFileDto) {
+    return this.filesService.delete(id, query.userId);
+  }
 
   //Reading file by id
   @Get('/:fileId')
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,25 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import mongoose, { Types } from 'mongoose';
 import { Files } from 'src/schemas/files.schema';
+import { UploadFileDto } from './dtos/upload-file.dto';
 
 @Injectable()
 export class FilesService {
   constructor(
-    @InjectModel(Files.name) private fileModel: mongoose.Model<Files>,
+    @InjectModel(Files.name) private filesModel: mongoose.Model<Files>,
   ) {}
 
-  getFileById(fileId: any) {
+  getFileById(fileId: string) {
     const objectId = new Types.ObjectId(fileId);
-    return this.fileModel.find({ _id: objectId });
+    return this.filesModel.find({ _id: objectId });
   }
 
   updateFile(fileId: string, userId: string) {
     const objectId = new Types.ObjectId(fileId);
     const userObjectId = new Types.ObjectId(userId);
-    return this.fileModel.updateOne(
+    return this.filesModel.updateOne(
       { _id: objectId },
       { $set: { updatedBy: userObjectId } },
     );
+  }
+
+  async upload(file: any, body: UploadFileDto) {
+    try {
+      const userIdObject = new Types.ObjectId(body.userId);
+      const uploadPayload = {
+        fileName: file.filename,
+        filePath: file.destination,
+        fileType: file.originalname?.split('.')[1],
+        targettedStorage: 'LocalStorage',
+        userId: userIdObject,
+        createdBy: userIdObject,
+      };
+      return this.filesModel.create(uploadPayload);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async delete(id: string, userId: string) {
+    try {
+      const fileIdObject = new Types.ObjectId(id);
+      const userIdObject = new Types.ObjectId(userId);
+      const file = await this.filesModel.findById(fileIdObject);
+      if (!file) {
+        throw new NotFoundException('File not found');
+      }
+      const updatedValue = await this.filesModel.updateOne(
+        { _id: fileIdObject },
+        { isDeleted: true, deletedBy: userIdObject, deletedAt: new Date() },
+      );
+      return updatedValue;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
