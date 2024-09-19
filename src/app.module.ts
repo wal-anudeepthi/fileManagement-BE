fc4bfd66ce220f2c77f8a2067e03857ab1832116
wal-anudeepthi <anudeepthi.k@veltris.com>
2024-09-19 13:11:25 +0530

    Using ServeStaticModule to get images

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,18 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { APP_PIPE } from '@nestjs/core';
 import { FilesModule } from './files/files.module';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
     }),
     MongooseModule.forRoot(process.env.DB_URI),
     UsersModule,
